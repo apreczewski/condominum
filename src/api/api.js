033ba5710d2f944874c82ab6506c4d0af3b0e928
Alexandre Preczewski <alexandrep@outboxup.com>
2021-10-13 19:30:45 -0300

    merge and update publications

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Toast from 'react-native-toast-message';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { BASE_API } from '../config/environment';
 
 const api = axios.create({
@@ -12,10 +13,8 @@ const api = axios.create({
 });
 
 const getToken = async () => {
-	const userToken =
-		'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwdWJsaWNfaWQiOiIwYTZmYTZkZi05MTUwLTQ3ZDMtYTQ0ZC00Njg3MzI4MWExMzgiLCJleHAiOjE2Njk5MTM0MDh9.HnQhal-wdj1nie9BCCuzqgkxc3Jf-ptt5jZ1NK3qQSQ';
-	// let userToken = await AsyncStorage.getItem('userToken');
-	// userToken = JSON.parse(userToken);
+	let userToken = await AsyncStorage.getItem('userToken');
+	userToken = JSON.parse(userToken);
 
 	if (userToken) {
 		api.defaults.headers.common['x-access-token'] = `${userToken}`;
